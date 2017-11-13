54663afabf644564f36609ecfd59fdfb01aba4a9
c125011501 <33483245+c125011501@users.noreply.github.com>
2017-11-13 19:38:54 +0800

    Add files via upload

--- geolocation.js
+++ geolocation.js
@@ -1,13 +1,21 @@
-window.onclick = function () {
+window.onload = function () {
     if (navigator.geolocation) {
+        var center;
         navigator.geolocation.getCurrentPosition(function (position) {
             document.getElementById("location").innerHTML =
                 "<h2>Latitude: " + position.coords.latitude +
                 "<br>Longitude: " + position.coords.longitude + "</h2>";
+            center = {lat: parseInt(position.coords.latitude), lng: parseInt(position.coords.longitude)};
         });
-        document.getElementById("accuracy").innerHTML = "sdasdasd"
+
+        var map = new google.maps.Map(document.getElementById('accuracy'), {
+            zoom: 14,
+            center: center
+        });
+
     } else {
         document.getElementById("location").innerHTML = "<h1>Your" +
-            " browser does not support geolocation.</h1>"
+            " browser does not support geolocation.</h1>";
+        document.getElementById("accuracy").innerHTML = "Failed"
     }
 };
